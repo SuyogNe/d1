b9c792097bf014e16e2b209bf4f27bd807312e96
SuyogNe <suyogn78@gmail.com>
2025-02-03 21:52:55 +0545

    d1

---
+++ d1.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+print("My Name Is Suyog")
+print('a = 10')
+print('b = 20')
+print('a + b =', 10 + 20)
+print('a - b =', 10 - 20)
+print('a * b =', 10 * 20)
+print("\n I am learning Python from codingal \n")
